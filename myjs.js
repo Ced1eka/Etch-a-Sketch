1fa169c458cba8989162362338e15c020ab2bda9
Ced <cedieka2@gmail.com>
2023-09-13 03:31:41 +0900

    First commint

---
+++ myjs.js
@@ -0,0 +1,6 @@
+const containerDiv = document.querySelector('.container');
+
+const div = document.createElement('div');
+containerDiv.appendChild(div);
+
+div.textContent = 'test to make sure everyting is wired up correctly';

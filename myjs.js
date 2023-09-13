44a5a406129847043e3d33e3c11a57bbb88d4f03
Ced <cedieka2@gmail.com>
2023-09-14 04:57:59 +0900

    Finally made some progress.

--- myjs.js
+++ myjs.js
@@ -1,10 +1,11 @@
 const containerDiv = document.querySelector('.container');
 
+      for (let i = 0; i < 256; i++ ){
+            const newDiv = document.createElement('div');
+            newDiv.setAttribute('class', 'square');
 
-for (let i = 0; i < 256; i++ ){
-      const newDiv = document.createElement('div');
-      newDiv.textContent = `${i+1}`;
-      containerDiv.appendChild(newDiv);
+            newDiv.textContent = `${i+1}`;
+            containerDiv.appendChild(newDiv);
 
 
-}
+      }

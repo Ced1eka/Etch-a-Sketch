d1c8e050daaf01e85ddd2f4bfacb27bc03f8315a
Ced <cedieka2@gmail.com>
2023-09-15 03:11:00 +0900

    Finished making the right grid using flexbox.
    Still need to implement generate grid button
    and add some styles.

--- myjs.js
+++ myjs.js
@@ -1,10 +1,15 @@
 const containerDiv = document.querySelector('.container');
 
-      for (let i = 0; i < 256; i++ ){
+const sizeOfGrid = prompt("How many square?", 0);
+const squareSize = ((960 / Number(sizeOfGrid)) - 0.5 ) + 'px';
+
+
+      for (let i = 0; i < sizeOfGrid * sizeOfGrid; i++ ){
             const newDiv = document.createElement('div');
             newDiv.setAttribute('class', 'square');
-
-            newDiv.textContent = `${i+1}`;
+            newDiv.style.width = squareSize;
+            newDiv.style.height = squareSize;
+            // newDiv.textContent = `${i+1}`;
             containerDiv.appendChild(newDiv);
 
 
